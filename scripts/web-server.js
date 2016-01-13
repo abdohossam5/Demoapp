4863421ccd9e0c8ea6b221104e3655767b64259d
abdohossam5 <abdohossam5@gmail.com>
2016-01-14 01:57:33 +0800

    up

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var path = require('path');
-var evens = require('./eventsController');
+var events = require('./eventsController');
 var app = express();
 var rootPath= path.normalize(__dirname + '/../');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(rootPath + '/app'));
 
 app.get('/data/event/:id', events.get);
